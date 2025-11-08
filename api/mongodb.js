69b9321ba698f57e39f6a8ac23d4db783bc1fd8a
Anant Bhatnagar <162437296+ig-crysis@users.noreply.github.com>
2025-11-08 22:44:42 +0530

    Update mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,1 +1,23 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URI;
+let client;
+let clientPromise;
+
+if (!process.env.MONGO_URI) {
+  throw new Error("Please add your Mongo URI to Environment Variables");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
 
